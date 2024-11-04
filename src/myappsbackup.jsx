6ca6bdc38f6999552ea167a9b1eb8348199b8d82
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-11-04 17:17:28 -0600

    Adding Tiles

--- src/myappsbackup.jsx
+++ src/myappsbackup.jsx
@@ -60,38 +60,29 @@ function MyApps() {
       <div id="bottom"></div>
 
       <div className="columns">
-        <div className="tile is-ancestor">
-          <div className="tile is-6 is-parent">
-            <div className="tile is-child box">
-
-              <h1 className="headline">Projects</h1>
-              <ul>
-                <li>- <a href="https://github.com/dcurtin91/stock-market-summary" target="_blank" title="AI Powered Stock Market Summarizer">AI Powered Stock Market Summarizer</a></li>
-                <li>- <a href="https://github.com/dcurtin91/couchsurf-network" target="_blank" title="CouchSurf Network - Member Portal">CouchSurf Network - Member Portal</a></li>
-                <li>- <a href="https://github.com/dcurtin91/spec-check-new" target="_blank" title="Spec Check">Spec Check</a></li>
-                <li>- <a href="https://github.com/dcurtin91/gpt-loop" target="_blank" title="GPT Loop">GPT Loop</a></li>
-                <li>- <a href="https://dcurtin91.pythonanywhere.com/" target="_blank" title="Central Spectroid">Central Spectroid</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="tile is-6 is-parent">
-          <div className="tile is-child box">
-
+        <div className="column is-5">
+          
+        <h1 className="headline">Projects</h1>
+          <ul>
+            <li>- <a href="https://github.com/dcurtin91/stock-market-summary" target="_blank" title="AI Powered Stock Market Summarizer">AI Powered Stock Market Summarizer</a></li>
+            <li>- <a href="https://github.com/dcurtin91/couchsurf-network" target="_blank" title="CouchSurf Network - Member Portal">CouchSurf Network - Member Portal</a></li>
+            <li>- <a href="https://github.com/dcurtin91/spec-check-new" target="_blank" title="Spec Check">Spec Check</a></li>
+            <li>- <a href="https://github.com/dcurtin91/gpt-loop" target="_blank" title="GPT Loop">GPT Loop</a></li>
+            <li>- <a href="https://dcurtin91.pythonanywhere.com/" target="_blank" title="Central Spectroid">Central Spectroid</a></li>
+          </ul>
+          
           <h1 className="headline" style={{ marginTop: "60px" }}>Work Experience</h1>
           <ul>
-            <li>- Customer Success Engineer @ Opendate (2024 - Present)</li>
+          <li>- Customer Success Engineer @ Opendate (2024 - Present)</li>
             <li>- Product Support @ WeWork (2023 - 2024)</li>
             <li>- Digital Events @ Mandolin Software (2021 - 2023)</li>
             <li>- Digital Events @ NoonChorus (2020 - 2021)</li>
           </ul>
-          </div>
-          </div>
+          
+         
         </div>
 
-        <div className="tile is-ancestor">
-          <div className="tile is-6 is-parent">
-            <div className="tile is-child box">
+        <div className="column is-1"></div>
 
         <div className="column is-6" style={{ marginLeft: "50px" }}>
           <h1 className="headline">Tech Certifications</h1>
@@ -102,18 +93,12 @@ function MyApps() {
             <li className="fcc">JavaScript Algorithms and Data Structures - <a href="https://www.freecodecamp.org/certification/dcurtin/javascript-algorithms-and-data-structures-v8" title="JS Algorithms Cert" target="_blank">Certificate</a></li>
             <li className="fcc">Responsive Web Design - <a href="https://www.freecodecamp.org/certification/dcurtin/responsive-web-design" title="Web Design Cert" target="_blank">Certificate</a></li>
           </ul>
-          </div>
-          </div>
-          <div className="tile is-6 is-parent">
-            <div className="tile is-child box">
+
           <h1 className="headline">More...</h1>
           <ul>
             <li>- Send me an <a href="/contact">email</a></li>
             <li>- Check out my <a href="/music">music</a></li>
           </ul>
-          </div>
-          </div>
-        </div>
         </div>
       </div>
     </div>
