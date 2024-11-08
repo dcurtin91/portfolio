c5e25c90a0fb7601453684b7dca0119b48d2704f
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-11-08 16:29:38 -0600

    fix menu

--- src/Music.jsx
+++ src/Music.jsx
@@ -1,22 +1,22 @@
 function Music() {
     
     return (
-        <div>
+        <div style= {{ padding: "20px"}}>
             <div className="tile is-ancestor">
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
                         <iframe className="bandcamp" title="bandcamp" src="https://bandcamp.com/EmbeddedPlayer/album=479054567/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/album/gas">Gas by David Curtin</a></iframe>
                     </div>
                 </div>
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
 
                         <iframe className="bandcamp" title="fruits" src="https://bandcamp.com/EmbeddedPlayer/track=3463874471/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/track/damp-melody">Damp Melody by David Curtin</a></iframe>
 
                     </div>
                 </div>
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
                         <iframe className="bandcamp" title="bandcamp" src="https://bandcamp.com/EmbeddedPlayer/track=2344229829/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/track/rocky-breaks">Rocky Breaks by David Curtin</a></iframe>
                     </div>
                 </div>
@@ -24,19 +24,19 @@ function Music() {
             </div>
             <div className="tile is-ancestor">
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
                         <iframe className="bandcamp" title="bandcamp" src="https://bandcamp.com/EmbeddedPlayer/track=3904961062/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/track/dave11">Dave11 by David Curtin</a></iframe>
                     </div>
                 </div>
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
 
                         <iframe className="bandcamp" title="fruits" src="https://bandcamp.com/EmbeddedPlayer/album=2974618601/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://woongi.bandcamp.com/album/fruits-of-the-midi">Fruits of the Midi by Woongi</a></iframe>
 
                     </div>
                 </div>
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
                         <iframe className="bandcamp" title="bandcamp" src="https://bandcamp.com/EmbeddedPlayer/album=3408169599/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/album/cms">CMS by David Curtin</a></iframe>
                     </div>
                 </div>
@@ -44,19 +44,19 @@ function Music() {
             </div>
             <div className="tile is-ancestor">
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
                         <iframe className="bandcamp" title="bandcamp" src="https://bandcamp.com/EmbeddedPlayer/album=3168018172/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/album/cms">CMS by David Curtin</a></iframe>
                     </div>
                 </div>
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
 
                         <iframe className="bandcamp" title="fruits" src="https://bandcamp.com/EmbeddedPlayer/album=1149480699/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://woongi.bandcamp.com/album/fruits-of-the-midi">Fruits of the Midi by Woongi</a></iframe>
 
                     </div>
                 </div>
                 <div className="tile is-4 is-parent">
-                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} >
+                    <div className="tile is-child box" style={{ display: "flex", justifyContent: "center" }} id="music">
                         <iframe className="bandcamp" title="rips" src="https://bandcamp.com/EmbeddedPlayer/album=931397215/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://woongi.bandcamp.com/album/rips-cuts">Rip&#39;s Cuts by Woongi</a></iframe>
                     </div>
                 </div>
